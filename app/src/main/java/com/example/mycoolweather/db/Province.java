0169a4cdec8902e0fb4efab152846873aa5321ed
chengjiangxu <chengjiangxu@cmhi.chinamobile.com>
2021-12-03 10:48:57 +0800

    OCT db

---
+++ app/src/main/java/com/example/mycoolweather/db/Province.java
@@ -0,0 +1,35 @@
+package com.example.mycoolweather.db;
+
+import org.litepal.exceptions.DataSupportException;
+
+import java.io.Serializable;
+
+public class Province implements Serializable {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
