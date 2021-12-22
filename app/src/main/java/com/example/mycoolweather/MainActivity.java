cff3751819e30860def7fda25433dfa401f1920e
chengjiangxu <chengjiangxu@cmhi.chinamobile.com>
2021-12-22 17:19:38 +0800

    OCT branch测试

--- app/src/main/java/com/example/mycoolweather/MainActivity.java
+++ app/src/main/java/com/example/mycoolweather/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private int mInt = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
